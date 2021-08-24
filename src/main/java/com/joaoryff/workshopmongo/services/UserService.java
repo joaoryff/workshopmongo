01b8f9a616f47758181029ea683e21c7bd0f8c43
joaoryff <joaoryff@gmail.com>
2021-08-24 17:37:07 -0400

    Found the User by Id and error treatment

--- src/main/java/com/joaoryff/workshopmongo/services/UserService.java
+++ src/main/java/com/joaoryff/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.joaoryff.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.joaoryff.workshopmongo.domain.User;
 import com.joaoryff.workshopmongo.repository.UserRepository;
+import com.joaoryff.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,5 +20,13 @@ public class UserService {
 	public List<User>findAll(){
 		return repo.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+
+	
+	
 	
 }
